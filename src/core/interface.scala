6c9bb28afc4d2a552245b92d6078343709aef77c
Kacper Korban <kacper.f.korban@gmail.com>
2021-06-12 03:07:19 +0200

    Params hopefully

--- src/core/interface.scala
+++ src/core/interface.scala
@@ -8,9 +8,13 @@ case class TypeInfo(owner: String, short: String, typeParams: Iterable[TypeInfo]
   def full: String = s"$owner.$short"
 
 object CaseClass:
-  trait Param[Typeclass[_], Type](val label: String, val index: Int,
-      val repeated: Boolean, val annotations: IArray[Any], val typeAnnotations: IArray[Any]):
-    
+  trait Param[Typeclass[_], Type](
+    val label: String,
+    val index: Int,
+    val repeated: Boolean,
+    val annotations: IArray[Any],
+    val typeAnnotations: IArray[Any]
+  ):
     type PType
     
     def typeclass: Typeclass[PType]
@@ -19,10 +23,16 @@ object CaseClass:
     override def toString: String = s"Param($label)"
   
   object Param:
-    def apply[F[_], T, P](name: String, idx: Int, repeated: Boolean, cbn: CallByNeed[F[P]],
-        defaultVal: CallByNeed[Option[P]], annotations: IArray[Any],
-        typeAnnotations: IArray[Any]): Param[F, T] =
-      new CaseClass.Param[F, T](name, idx, repeated, annotations, typeAnnotations):
+    def apply[Typeclass[_], T, P](
+      name: String,
+      idx: Int,
+      repeated: Boolean,
+      cbn: CallByNeed[Typeclass[P]],
+      defaultVal: CallByNeed[Option[P]],
+      annotations: IArray[Any],
+      typeAnnotations: IArray[Any]
+    ): Param[Typeclass, T] =
+      new CaseClass.Param[Typeclass, T](name, idx, repeated, annotations, typeAnnotations):
         type PType = P
         def default: Option[PType] = defaultVal.value
         def typeclass = cbn.value
@@ -31,13 +41,14 @@ object CaseClass:
 end CaseClass
 
 
-abstract class CaseClass[Typeclass[_], Type]
-                        (val typeInfo: TypeInfo,
-                         val isObject: Boolean,
-                         val isValueClass: Boolean,
-                         val params: IArray[CaseClass.Param[Typeclass, Type]],
-                         val annotations: IArray[Any],
-                         val typeAnnotations: IArray[Any]) extends Serializable:
+abstract class CaseClass[Typeclass[_], Type](
+  val typeInfo: TypeInfo,
+  val isObject: Boolean,
+  val isValueClass: Boolean,
+  val params: IArray[CaseClass.Param[Typeclass, Type]],
+  val annotations: IArray[Any],
+  val typeAnnotations: IArray[Any]
+) extends Serializable:
 
   type Param = CaseClass.Param[Typeclass, Type]
   
@@ -47,9 +58,15 @@ abstract class CaseClass[Typeclass[_], Type]
   def constructEither[Err, PType: ClassTag](makeParam: Param => Either[Err, PType]): Either[List[Err], Type]
   def rawConstruct(fieldValues: Seq[Any]): Type
 
-  def param[P](name: String, idx: Int, repeated: Boolean, cbn: CallByNeed[Typeclass[P]],
-      defaultVal: CallByNeed[Option[P]], annotations: IArray[Any],
-      typeAnnotations: IArray[Any]): Param =
+  def param[P](
+    name: String,
+    idx: Int,
+    repeated: Boolean,
+    cbn: CallByNeed[Typeclass[P]],
+    defaultVal: CallByNeed[Option[P]],
+    annotations: IArray[Any],
+    typeAnnotations: IArray[Any]
+  ): Param =
     new CaseClass.Param[Typeclass, Type](name, idx, repeated, annotations, typeAnnotations):
       type PType = P
       def default: Option[PType] = defaultVal.value
@@ -57,11 +74,12 @@ abstract class CaseClass[Typeclass[_], Type]
       def deref(value: Type): P = value.asInstanceOf[Product].productElement(idx).asInstanceOf[P]
 end CaseClass
 
-case class SealedTrait[Typeclass[_], Type]
-                      (typeInfo: TypeInfo,
-                       subtypes: IArray[SealedTrait.Subtype[Typeclass, Type, _]],
-                       annotations: IArray[Any],
-                       typeAnnotations: IArray[Any]) extends Serializable:
+case class SealedTrait[Typeclass[_], Type](
+  typeInfo: TypeInfo,
+  subtypes: IArray[SealedTrait.Subtype[Typeclass, Type, _]],
+  annotations: IArray[Any],
+  typeAnnotations: IArray[Any]
+) extends Serializable:
 
   type Subtype[S] = SealedTrait.SubtypeValue[Typeclass, Type, S]
 
@@ -80,16 +98,16 @@ case class SealedTrait[Typeclass[_], Type]
 end SealedTrait
 
 object SealedTrait:
-  class Subtype[Typeclass[_], Type, SType]
-               (val typeInfo: TypeInfo,
-                val annotations: IArray[Any],
-                val typeAnnotations: IArray[Any],
-                val isObject: Boolean,
-                index: Int,
-                callByNeed: CallByNeed[Typeclass[SType]],
-                isType: Type => Boolean,
-                asType: Type => SType & Type
-               ) extends PartialFunction[Type, SType & Type], Serializable:
+  class Subtype[Typeclass[_], Type, SType](
+    val typeInfo: TypeInfo,
+    val annotations: IArray[Any],
+    val typeAnnotations: IArray[Any],
+    val isObject: Boolean,
+    index: Int,
+    callByNeed: CallByNeed[Typeclass[SType]],
+    isType: Type => Boolean,
+    asType: Type => SType & Type
+  ) extends PartialFunction[Type, SType & Type], Serializable:
     def typeclass: Typeclass[SType & Type] = callByNeed.value.asInstanceOf[Typeclass[SType & Type]]
     def cast: PartialFunction[Type, SType & Type] = this
     def isDefinedAt(t: Type): Boolean = isType(t)
